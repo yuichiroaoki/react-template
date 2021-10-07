25a959d91244958a0db94b6af3b58bc34a2cc6b4
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-10-07 11:40:09 +0900

    react router dom

---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
